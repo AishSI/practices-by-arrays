0c6913ffeb45cab67fd8c08f4432694117a6ca3c
AishSI <aishsi@gmail.com>
2018-11-26 21:53:10 +0700

    Add project files.

---
+++ HeatmapTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        enum Month
+        {
+            Jan,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul,
+            Aug,
+            Sep,
+            Oct,
+            Nov,
+            Dec
+        }
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            //var proba = Month;
+            var dayMonth = new string[30];
+            var monthYear = new string[12];
+            var peopleBornOfTheDay = new double[30, 12];
+            for (var x = 0; x < dayMonth.Length; x++)
+                dayMonth[x] = (x + 2).ToString();
+            for (var y = 0; y < monthYear.Length; y++)
+                monthYear[y] = Enum.Format(typeof(Month), y, "F");
+            foreach (var birthday in names)
+            {
+                // это решение практики "Тепловая карта"
+                //if (birthday.BirthDate.Day > 1)
+                //    peopleBornOfTheDay[birthday.BirthDate.Day - 2, birthday.BirthDate.Month - 1]++;
+
+                //Просто поискал Иванов с ДР 03.11
+                if (birthday.BirthDate.Day == 3)
+                    if (birthday.BirthDate.Month == 11)
+                        if (birthday.Name == "иван")
+                            peopleBornOfTheDay[birthday.BirthDate.Day - 2, birthday.BirthDate.Month - 1]++;
+            }
+            return new HeatmapData("Пример карты интенсивностей", peopleBornOfTheDay, dayMonth, monthYear);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {                
+            var dayMonth = new string[31];
+            for (var x = 0; x < dayMonth.Length; x++)
+                dayMonth[x] = (x + 1).ToString();
+            var samplingName = new double[31];
+            foreach (var nameFind in names)
+            {
+                if (nameFind.Name == name && nameFind.BirthDate.Day != 1) 
+                samplingName[nameFind.BirthDate.Day-1]++;
+            }
+
+            //return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), new string[0], new double[0]);
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), dayMonth, samplingName);
+        }
+    }
+}
